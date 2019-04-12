ab9a49b4f5b480d88e5859963dc08a9b1730b106
MrToph <christoph.michel.1992@gmail.com>
2019-04-12 15:59:24 +0200

    Updates

--- precache-manifest.4e52e2009cea558218c24be9456fe13b.js
+++ precache-manifest.4e52e2009cea558218c24be9456fe13b.js
@@ -1,30 +1,30 @@
 self.__precacheManifest = [
   {
-    "revision": "8f60d66525a6187080e9",
+    "revision": "b3c0b96de66723f2ba9e",
     "url": "./static/css/main.ac6d9f97.chunk.css"
   },
   {
-    "revision": "8f60d66525a6187080e9",
-    "url": "./static/js/main.6fc57567.chunk.js"
+    "revision": "b3c0b96de66723f2ba9e",
+    "url": "./static/js/main.69515b2c.chunk.js"
   },
   {
     "revision": "9eb600ee07a27cdad64f",
     "url": "./static/js/runtime~main.2c977d9b.js"
   },
   {
-    "revision": "9eac995eb183cc268cff",
+    "revision": "9c24ad05a568a0f5061a",
     "url": "./static/css/2.45f59960.chunk.css"
   },
   {
-    "revision": "9eac995eb183cc268cff",
-    "url": "./static/js/2.d18357a1.chunk.js"
+    "revision": "9c24ad05a568a0f5061a",
+    "url": "./static/js/2.7b585d59.chunk.js"
   },
   {
     "revision": "bbb6b67e852e88f20889d8b156ac5019",
     "url": "./static/media/github.bbb6b67e.svg"
   },
   {
-    "revision": "147c6d942fab45698b631ef088014630",
+    "revision": "49b576853c624035f97c038c4839b446",
     "url": "./index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5e618c95110dfb717ed6a8e5d4eff263.js"
+  "./precache-manifest.4e52e2009cea558218c24be9456fe13b.js"
 );
 
 workbox.clientsClaim();
